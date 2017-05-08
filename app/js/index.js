5a2a3976d8f77b02b285eb859b5b01fa690a86f2
Quinton Jason <quinton.jason@gmail.com>
2017-05-08 07:52:31 -0500

    finished sortable / added flexbox

--- app/js/index.js
+++ app/js/index.js
@@ -268,13 +268,46 @@ videojs.plugin('playList', playList);
 // demoModule.init();
 
 $(function  () {
-  var videoWrapper = $('.video-snippets .btn');
+  var videoWrapper = $('.video-snippets .btn:not(.hot-videos)');
   var hotVideoTrigger = $('#hot-videos');
   var hotVideos = $('.video-snippets .btn.on');
   $("ol.sortable-list").sortable({
-    onDrop: function(){
+    group: 'sortable-list',
+    pullPlaceholder: false,
+    // animation on drop
+    onDrop: function  ($item, container, _super) {
+      var $clonedItem = $('<li/>').css({height: 0});
+      $item.before($clonedItem);
+      $clonedItem.animate({'height': $item.height()});
+
+      $item.animate($clonedItem.position(), function  () {
+        $clonedItem.detach();
+        _super($item, container);
+      });
       getVideos();
+    },
+
+    // set $item relative to cursor position
+    onDragStart: function ($item, container, _super) {
+      var offset = $item.offset(),
+          pointer = container.rootGroup.pointer;
+
+      adjustment = {
+        left: pointer.left - offset.left,
+        top: pointer.top - offset.top
+      };
+
+      _super($item, container);
+    },
+    onDrag: function ($item, position) {
+      $item.css({
+        left: position.left - adjustment.left,
+        top: position.top - adjustment.top
+      });
     }
+    // onDrop: function(){
+    //   // getVideos();
+    // }
   });
   //Add classes to selected Videos
   videoWrapper.on('click', function(e){
